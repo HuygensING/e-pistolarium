40180c08ae717f8b4ae63abcf63d79ddfd63dba8
Gijsjan <agijsbro@gmail.com>
2017-11-30 13:21:35 +0100

    Pull janus because of extra build step

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 eval $(docker-machine env epistolarium)
+docker pull huygensing/janus
 npm run compose-prod -- pull
 npm run prod -- --build -d
 eval $(docker-machine env --unset)
