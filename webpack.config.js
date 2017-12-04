2174d64d1cd498b4ec71d13967e3644a3e3350cc
Gijsjan <agijsbro@gmail.com>
2017-12-04 23:35:06 +0100

    Fix docker build

--- webpack.config.js
+++ webpack.config.js
@@ -6,35 +6,32 @@ const outputDir = 'build'
 
 const plugins = []
 if (process.env.NODE_ENV === 'production') {
-	plugins.push(
-		new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
-		new UglifyJsPlugin(),
-	)
+	plugins.push(new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }))
+	plugins.push(new UglifyJsPlugin())
 } 
 
 module.exports = {
 	entry: "./src/index.tsx",
 	output: {
-			chunkFilename: `./${outputDir}/[name].bundel-${pkg.version}.js`,
-			filename: `./${outputDir}/bundle-${pkg.version}.js`,
+		chunkFilename: `./${outputDir}/[name].bundel-${pkg.version}.js`,
+		filename: `./${outputDir}/bundle-${pkg.version}.js`,
 	},
 	plugins,
 	resolve: {
-			// Add '.ts' and '.tsx' as resolvable extensions.
-			extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+		// Add '.ts' and '.tsx' as resolvable extensions.
+		extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
 	},
 	module: {
-			loaders: [
-					// All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-					{
-						test: /\.tsx?$/,
-						loader: "awesome-typescript-loader",
-					}
-			]
+		loaders: [
+			// All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+			{
+				test: /\.tsx?$/,
+				loader: "awesome-typescript-loader",
+			}
+		]
 	},
 	externals: {
 		react: "React",
 		"react-dom": "ReactDOM",
 	},
-};
-
+}
