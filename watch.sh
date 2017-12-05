396c88dfee7c1266c5433b39c4684c4cef72f055
Gijsjan <agijsbro@gmail.com>
2017-12-05 11:43:23 +0100

    Add react libs

--- watch.sh
+++ watch.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 rm -rf build
-node_modules/.bin/webpack -w
+mkdir build
 cp node_modules/react/dist/react.js build
 cp node_modules/react-dom/dist/react-dom.js build
+node_modules/.bin/webpack -w
