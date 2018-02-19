335da777eca10a30100b4f2c50de5ec0f6d509dc
Gijsjan <agijsbro@gmail.com>
2018-02-19 16:33:51 +0100

    Remove static_local/js from version control

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-# rm -rf build
+rm -rf static_local/js
 NODE_ENV=production node_modules/.bin/webpack
 node_modules/.bin/tsc -p tsconfig.server.json
 # cp node_modules/react/dist/react.js build
