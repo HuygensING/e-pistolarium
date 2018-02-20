72bbcb94e676579650fbbde86eea42cac6f89cf9
Gijsjan <agijsbro@gmail.com>
2018-02-20 14:38:18 +0100

    Remove dynamic dir 'server'

--- server/components/document2/header/prevnext/link-to-root-annotation.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const utils_1 = require("../../../../utils");
-const RootAnnotationLink = (props) => React.createElement("button", { onClick: (ev) => {
-        utils_1.changeLocation(`/documents/${props.annotation.id}`);
-    } }, props.children);
-exports.default = RootAnnotationLink;
--- server/utils.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.changeLocation = (location) => { window.location.href = location; };
