45622147ec9ddd7350d4449e75d8e5302a27f64b
Gijsjan <agijsbro@gmail.com>
2018-02-21 20:20:10 +0100

    Clean up docker dir

--- scripts/test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
-docker-compose -p correspondence-test -f docker/production.yml pull
-docker-compose -p correspondence-test -f docker/production.yml up --build -d
